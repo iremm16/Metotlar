bb8213359a14bb3509400c7326b8a712403160ef
iremm16 <77540346+iremm16@users.noreply.github.com>
2021-01-22 19:50:31 +0300

    Add files via upload

---
+++ SepetManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlarr
+{
+    class SepetManager
+    {
+        public void Ekle(Product product)
+		{
+            Console.WriteLine("Tebrikler. Sepete eklendi: " + product.Adi);
+
+		}
+
+        public void Ekle2(string ad, string aciklama, double fiyat, int stokAdedi)
+		{
+            Console.WriteLine("Tebrikler. Sepete eklendi: " + ad);
+        }
+    }
+}
